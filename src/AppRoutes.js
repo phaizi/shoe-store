70822d4532cab4a7aea41f513c350de9d3ec2973
FaizanM <faizan.mansur87@gmail.com>
2021-01-05 03:07:21 +0500

    Added Cart to AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,18 +4,19 @@ import Home from './components/Home'
 import About from './components/About'
 import Products from './components/Products'
 import ProductDetails from './components/ProductDetails'
+import Cart from './components/Cart'
 import Layout from './components/Layout'
 
 export default function AppRoutes() {
   return (
       <div>
-
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route path="/" element={<Home/>} />
         <Route path="products" element={<Products/>} />
         <Route path="products/:productID" element={<ProductDetails/>}/>
         <Route path="about" element={<About/>} />
+        <Route path="cart" element={<Cart/>} />
         <Route path='*' element= {<NotFound/>} />
       </Route>
     </Routes>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom';
 
 
-export default function NotFound(){
+export default function Layout(){
     return (
       <div>
         <nav>
         <Link to="/">Home</Link> {' '}
         <Link to="/products">Products</Link> {' '}
-        <Link to="/about">About</Link>
+        <Link to="/about">About</Link> {' '}
+        <Link to="/cart">Cart</Link>
       </nav>
       <Outlet/>
       </div>
