c84452d8738fd0aaf6e6cad1dfb3bb865beebf76
FaizanM <faizan.mansur87@gmail.com>
2021-01-05 21:39:02 +0500

    removed warnings

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
+// import { Routes, Route, Link } from 'react-router-dom';
 
 export default function Home() {
   return (
